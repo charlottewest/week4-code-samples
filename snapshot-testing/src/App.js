3b2690131ade44a37d46ca56696f2cf802dd86c9
bhague1281 <bhague1281@gmail.com>
2019-04-30 20:33:59 -0700

    snapshot testing

---
+++ snapshot-testing/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function TestComponent() {
+  return (
+    <h1>Test component</h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <TestComponent />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
